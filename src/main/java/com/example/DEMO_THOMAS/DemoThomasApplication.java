ef638d7c2fc44996c62fb9dacff9c259986ae6dc
Thomas571999 <thomas.goncalves571999@gmail.com>
2026-02-04 12:06:21 +0100

    TP : remise à zéro du repo avec les fichiers actuels

---
+++ src/main/java/com/example/DEMO_THOMAS/DemoThomasApplication.java
@@ -0,0 +1,13 @@
+package com.example.DEMO_THOMAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThomasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DEMO_THOMAS/DemoThomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DEMO_THOMAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
